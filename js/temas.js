0b147a1ecd01fcadb5e8fd8f922737bcb3831891
Portal Colaborativo <coextechnology@gmail.com>
2023-08-29 21:59:57 -0300

    Commit del Portal

---
+++ js/temas.js
@@ -0,0 +1,7 @@
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
