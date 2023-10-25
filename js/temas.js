e2511fd09089033cb8a40971f096c1c9dbeebbca
Portal Corporativo <coextechnology@gmail.com>
2023-10-25 18:14:43 -0300

    commit portal 12 Maria

--- js/temas.js
+++ js/temas.js
@@ -2,6 +2,5 @@ const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () => {
 
-    document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 });
